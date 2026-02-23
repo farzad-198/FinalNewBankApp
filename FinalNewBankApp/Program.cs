ed678ae87cd976db4770c1de23d21e85a7f98a35
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-02-23 17:14:52 +0100

    Add project files.

---
+++ FinalNewBankApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalNewBankApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
