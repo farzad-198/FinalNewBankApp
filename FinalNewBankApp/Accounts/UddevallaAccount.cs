66f467284104c497909971bdf2e09d8d03bb5d30
farzad-198 <farzadjafarzadehtey@gmail.com>
2026-03-05 09:27:35 +0100

    chenck

--- FinalNewBankApp/Accounts/UddevallaAccount.cs
+++ FinalNewBankApp/Accounts/UddevallaAccount.cs
@@ -5,7 +5,9 @@ namespace FinalNewBankApp.Accounts
 {
     internal class UddevallaAccount : AccountBase
     {
-        protected UddevallaAccount() { }
+        protected UddevallaAccount() 
+        { 
+        }
         public UddevallaAccount(decimal startBalance, string accountName, string accountNumber, DateTime dateTime)
             : base(startBalance, accountName, accountNumber, dateTime)
         {
